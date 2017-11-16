f662d61984b537a6f3edc72fcdbaa3b3bd58bd0c
Gabriel Pająk <pajak.gabriel@gmail.com>
2017-11-16 20:20:33 +0100

    Init project.

---
+++ Bucket/Bucket/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bucket.Startup))]
+namespace Bucket
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
