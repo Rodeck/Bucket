3ec70db6d2844bf3b49ca91b168bf3a757788d40
Gabriel Pająk <pajak.gabriel@gmail.com>
2017-11-16 22:25:38 +0100

    Create basic view.

---
+++ Bucket/Bucket/DAL/BucketContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Bucket.DAL
+{
+    public class BucketContext: DbContext
+    {
+        public BucketContext() : base("DefaultConnection")
+        {
+        }
+    }
+}
